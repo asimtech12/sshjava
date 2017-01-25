4982c49235a2ed4c7f59ba07fd54ec3f1bf6187c
masim <masim@192.168.0.102>
2017-01-25 16:32:25 +0530

    removed the hard coded values added comments added log messages

--- src/main/java/com/sshconn/mysshconnector.java
+++ src/main/java/com/sshconn/mysshconnector.java
@@ -1,6 +1,5 @@
 package com.sshconn;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,50 +11,53 @@ import com.jcraft.jsch.Session;
 import com.jcraft.*;
 import org.apache.log4j.Logger;
 
-/**
- * Hello world!
- *
- */
+// this code will do the ssh and will take 3 parameters
 public class mysshconnector {
 	static Logger log = Logger.getLogger(mysshconnector.class.getName());
 
-	public static void main( String[] args )
-    {
-     
-try{
-	System.out.println( "Hello World!" );
-    // log.debug("Hello this is a debug message");
-    // log.info("Hello this is an info message");
-     JSch jsch=new JSch();
-     log.info("Connecting to server");
-     Session session=jsch.getSession("masim", "192.168.0.102", 22);
-     session.setPassword("Welcome#1234");
-     Properties config = new Properties();
-     config.put("StrictHostKeyChecking", "no");
-     session.setConfig(config);
-     session.connect();
-//this is asim
+	public static void main(String[] args) {
 
-     ChannelExec channel=(ChannelExec) session.openChannel("exec");
-     BufferedReader in=new BufferedReader(new InputStreamReader(channel.getInputStream()));
-     channel.setCommand("pwd;");
-     channel.connect();
+		try {
+			if (args.length < 3) {
+				log.error("please enter valid usage");
+				System.exit(1);
+			}
+			String hostname = args[0];
+			String username = args[1];
+			String password = args[2];
+			// log.debug("Hello this is a debug message");
+			// log.info("Hello this is an info message");
+			JSch jsch = new JSch();
+			log.info("Connecting to server");
+			Session session = jsch.getSession(username, hostname, 22);
+			session.setPassword(password);
+			Properties config = new Properties();
+			config.put("StrictHostKeyChecking", "no");
+			session.setConfig(config);
+			session.connect();
+			log.info("Connected to the server :::" + hostname);
 
-     String msg=null;
-     while((msg=in.readLine())!=null){
-       System.out.println(msg);
-     }
+			ChannelExec channel = (ChannelExec) session.openChannel("exec");
+			BufferedReader in = new BufferedReader(new InputStreamReader(channel.getInputStream()));
+			// printing the present working directory after logging into the host
+			channel.setCommand("pwd;");
+			channel.connect();
 
-     channel.disconnect();
-     session.disconnect();
-}
-//catch(JSchException  ex){
-//	log.error(ex);
-	
-//}
-catch(Exception allex){
-	log.error(allex);
-	
-}
-    }
+			String msg = null;
+			while ((msg = in.readLine()) != null) {
+				System.out.println(msg);
+			}
+
+			channel.disconnect();
+			session.disconnect();
+		}
+		// catch(JSchException ex){
+		// log.error(ex);
+
+		// }
+		catch (Exception allex) {
+			log.error(allex.getMessage());
+
+		}
+	}
 }
